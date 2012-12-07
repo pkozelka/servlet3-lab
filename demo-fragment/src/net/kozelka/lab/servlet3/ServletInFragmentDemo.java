df77b04315e6020db7238246347447fd1f47aa98
Petr Kozelka <pkozelka@gmail.com>
2012-12-07 17:47:24 +0100

    initial commit, servlet3 from fragment works, static resources dont

---
+++ demo-fragment/src/net/kozelka/lab/servlet3/ServletInFragmentDemo.java
@@ -0,0 +1,23 @@
+package net.kozelka.lab.servlet3;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author Petr Kozelka
+ */
+@WebServlet("/fragment/hello")
+public class ServletInFragmentDemo extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final PrintWriter pw = resp.getWriter();
+        pw.println("Hello it's me, the " + ServletInFragmentDemo.class + "!");
+        pw.flush();
+    }
+}
